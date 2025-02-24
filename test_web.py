ab31380718e2c888d958b0d6446a7e6c5f148ad4
wei <1419316318@qq.com>
2025-02-24 22:52:50 +0800

    添加新用例

---
+++ test_web.py
@@ -0,0 +1,13 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+def test_shop():
+
+    driver=webdriver.Chrome()
+    driver.get("http://www.baidu.com")
+    driver.find_element(By.CSS_SELECTOR,"#kw").send_keys("测试")
+    time.sleep(2)
+    driver.find_element(By.CSS_SELECTOR,"#su").click()
+    time.sleep(2)
+    driver.quit()
