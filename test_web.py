97a964d17177519847012ab70f9c929b0558f02c
wei-abc <1419316318@qq.com>
2025-02-25 19:38:08 +0800

    添加新用例

--- test_web.py
+++ test_web.py
@@ -6,7 +6,7 @@ def test_shop():
 
     driver=webdriver.Chrome()
     driver.get("http://www.baidu.com")
-    driver.find_element(By.CSS_SELECTOR,"#kw").send_keys("测试2")
+    driver.find_element(By.CSS_SELECTOR,"#kw").send_keys("测试23")
     time.sleep(2)
     driver.find_element(By.CSS_SELECTOR,"#su").click()
     time.sleep(2)
