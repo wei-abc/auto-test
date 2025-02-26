597098bcc326b1e1d6702b5e6bc9d575bcdd4540
wei-abc <1419316318@qq.com>
2025-02-26 21:30:03 +0800

    Update test_web.py
    自动化构建测试

--- test_web.py
+++ test_web.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 def test_shop():
-
+#这是一个自动化构建测试
     driver=webdriver.Chrome()
     driver.get("http://www.baidu.com")
     driver.find_element(By.CSS_SELECTOR,"#kw").send_keys("测试23")
